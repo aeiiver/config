b90a0db141c964b00e4ca57eabbc352456c64644
aeiiver <101661283+aeiiver@users.noreply.github.com>
2023-06-02 22:02:27 +0200

    yep

--- userland/nvim/nvim/lua/core/plugins/colorscheme.lua
+++ userland/nvim/nvim/lua/core/plugins/colorscheme.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       -- set colorscheme
       vim.cmd.colorscheme('melange')
+
       -- restore transparent background
       vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
--- userland/nvim/nvim/lua/core/plugins/telescope.lua
+++ userland/nvim/nvim/lua/core/plugins/telescope.lua
@@ -4,19 +4,15 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local b = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>fc', b.builtin)
-      vim.keymap.set('n', '<leader>fC', b.live_grep)
+      local function grep() b.grep_string({ search = vim.fn.input('grep: ') }) end
+
       vim.keymap.set('n', '<leader>ff', b.find_files)
-      vim.keymap.set('n', '<leader>fF', b.current_buffer_fuzzy_find)
-      vim.keymap.set('n', '<leader>fh', b.help_tags)
-      vim.keymap.set('n', '<leader>fm', b.man_pages)
-      vim.keymap.set('n', '<leader>fb', b.buffers)
+      vim.keymap.set('n', '<leader>fg', grep)
       vim.keymap.set('n', '<leader>fG', b.live_grep)
-      vim.keymap.set(
-        'n',
-        '<leader>fg',
-        function() b.grep_string({ search = vim.fn.input('grep: ') }) end
-      )
+      vim.keymap.set('n', '<leader>fb', b.buffers)
+      vim.keymap.set('n', '<leader>fh', b.help_tags)
+      vim.keymap.set('n', '<leader>fc', b.builtin)
+      vim.keymap.set('n', '<leader>fm', b.man_pages)
     end,
   },
 }
