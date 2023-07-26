366f397b88f604fe02df677941acfb758e3f172d
aeiiver <101661283+aeiiver@users.noreply.github.com>
2023-07-26 20:17:15 +0200

    i forgor

--- desktop/hypr/.config/hypr/scripts/waylock.sh
+++ desktop/hypr/.config/hypr/scripts/waylock.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-exec waylock -init-color 0x040000 -input-color 0x080808 -fail-color 0x201010 -fork-on-lock
+exec waylock \
+	-init-color 0x040000 \
+	-input-color 0x080808 \
+	-fail-color 0x201010 \
+	-fork-on-lock
