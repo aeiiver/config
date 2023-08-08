9de4e940ed74df018d3dee4deda62ce0b0431b1f
aeiiver <101661283+aeiiver@users.noreply.github.com>
2023-08-08 21:17:26 +0200

    letmecook

--- scripts/ensure-mimeapps.list.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -pv "${XDG_DATA_HOME:-$HOME/.local/share}"/applications
-ln -sv "${XDG_CONFIG_HOME:-$HOME/.config}"/mimeapps.list "${XDG_DATA_HOME:-$HOME/.local/share}"/applications/mimeapps.list
