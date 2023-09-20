02ceef2abd58e3e05ba67c60ab426fe03ee7af20
aeiiver <101661283+aeiiver@users.noreply.github.com>
2023-09-20 09:39:34 +0200

    shellrc: sort out the file's insides

--- misc/shellrc.sh
+++ misc/shellrc.sh
@@ -1,41 +1,39 @@
 # vi: foldmethod=marker
 
-# BASH {{{
+case "$SHELL" in
+*/bash)
+	HISTSIZE=200000
+	HISTCONTROL=ignoredups
+	HISTTIMEFORMAT='%F %T  '
+	shopt -s histappend
+	shopt -s hostcomplete
+	stty -ixon
+	;;
+*/zsh)
+	HISTSIZE=200000
+	SAVEHIST=200000
+	HISTFILE=~/.zsh_history
+	bindkey -e
+	;;
+esac
 
-HISTSIZE=200000
-HISTCONTROL=ignoredups
-HISTTIMEFORMAT='%F %T  '
-shopt -s histappend
-shopt -s hostcomplete
-stty -ixon
-
-# end BASH }}}
-
-# ZSH {{{
-
-HISTFILE=~/.zsh_history
-HISTSIZE=200000
-SAVEHIST=200000
-bindkey -e
-
-# }}}
-
-# SH {{{
-
-# XDG base directories
+### XDG base directories
+#
 export XDG_CACHE_HOME="$HOME"/.cache
 export XDG_CONFIG_HOME="$HOME"/.config
 export XDG_DATA_HOME="$HOME"/.local/share
 export XDG_STATE_HOME="$HOME"/.local/state
 
-# Environment variables
+### Preferred applications
+#
 export EDITOR=nvim
 export VISUAL=nvim
 export PAGER='less -R --use-color'
 export MANPAGER='less -R --use-color -Dd+c -Du+y'
 export MANROFFOPT='-P -c'
 
-# Aliases
+### Aliases
+#
 alias v='command $VISUAL'
 alias ls='command ls --color=auto'
 alias la='command ls -lA --color=auto'
@@ -43,16 +41,31 @@ alias grep='command grep --color=auto'
 alias diff='command diff --color=auto'
 alias ip='command ip --color=auto'
 
-# Utilities
+### Utilities
+#
 path_prepend() {
+	if [ ! -d "$1" ] || [ ! -r "$1" ]; then
+		echo "path_prepend: '$1' can't be read"
+		return 1
+	fi
 	case :$PATH: in
 	*:$1:*) return ;;
 	esac
 	export PATH="$1:$PATH"
 }
+
+path_append() {
+	if [ ! -d "$1" ] || [ ! -r "$1" ]; then
+		echo "path_append: '$1' can't be read"
+		return 1
+	fi
+	case :$PATH: in
+	*:$1:*) return ;;
+	esac
+	export PATH="$PATH:$1"
+}
+
 source_checked() {
 	# shellcheck disable=1090
 	[ -f "$1" ] && [ -r "$1" ] && . "$1"
 }
-
-# end SH }}}
