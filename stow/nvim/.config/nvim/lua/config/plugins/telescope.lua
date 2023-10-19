2550c919cad48fb311717104552eee32bf9a81ea
aeiiver <101661283+aeiiver@users.noreply.github.com>
2023-10-19 07:12:31 +0200

    nvim: telescope

--- stow/nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ stow/nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -1,11 +1,21 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+          return vim.fn.executable('make') == 1
+        end,
+      },
+    },
     config = function()
       local bu = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', bu.find_files, { desc = 'Find files' })
-      vim.keymap.set('n', '<leader>fg', bu.live_grep, { desc = 'Live grep' })
+      vim.keymap.set('n', '<leader>fs', bu.live_grep, { desc = 'Live grep' })
+      vim.keymap.set('n', '<leader>fg', bu.grep_string, { desc = 'Grep string' })
       vim.keymap.set('n', '<leader>fh', bu.help_tags, { desc = 'Help tags' })
       vim.keymap.set('n', '<leader>fc', bu.builtin, { desc = 'Telescope' })
 
@@ -17,6 +27,7 @@ return {
           },
         },
       })
+      pcall(require('telescope').load_extension, 'fzf')
     end,
   },
 }
