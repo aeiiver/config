75a8e93fb9f369deaea1dccaf66cbdb9321062cb
aeiiver <101661283+aeiiver@users.noreply.github.com>
2023-10-19 07:11:56 +0200

    nvim: move fidget spec into lsp

--- stow/nvim/.config/nvim/lua/config/plugins/lsp.lua
+++ stow/nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -169,6 +169,13 @@ return {
       'L3MON4D3/LuaSnip',
       'rafamadriz/friendly-snippets',
       'saadparwaiz1/cmp_luasnip',
+
+      {
+        'j-hui/fidget.nvim',
+        tag = 'legacy',
+        event = 'LspAttach',
+        opts = {},
+      },
     },
     config = function()
       prepare_cmp()
